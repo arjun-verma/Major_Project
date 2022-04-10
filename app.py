4ef456b2c0d7a95d3113c38e675091356f2c319c
arjun-verma <verma.arjun254@gmail.com>
2022-04-10 13:56:37 +0530

    flasked

--- app.py
+++ app.py
@@ -1,12 +1,31 @@
-# fapp -> shortcut to generate
+import pandas as pd
+import numpy as np
+import plotly.express as px
+import matplotlib.pyplot as plt
 from flask import Flask, render_template
+import json
+import plotly
+
 app = Flask(__name__)
+
 
 
 @app.route('/')
 def index():
+    dfs = pd.read_csv('datasets/Salary.csv')
+    dfs.drop(['Unnamed: 0'], axis = 1, inplace = True)
+    df = dfs.groupby(['Sport']).median()
+    fig = px.bar(df,x=df.index,y='Pay2014',color ='Pay2014',log_y=True,title="Pay 2014 median")
+    fig2 = px.bar(df,x=df.index,y='Salary_Winnings',color ='Pay2014',log_y=True,title="Salary Winning median")
+    graphJSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
+    graphJSON2 = json.dumps(fig2, cls=plotly.utils.PlotlyJSONEncoder)
+    return render_template('index.html', pay2014graph=graphJSON, salwingraph=graphJSON2)
 
-    return render_template('index.html')
+@app.route('/bivar')
+def bivar():
+  
+    return render_template('bivar.html')
 
 if __name__ == '__main__':
   app.run(host='127.0.0.1', port=8000, debug=True)
+ 
